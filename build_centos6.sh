b18d42bfed68be80041d641a5c94b7116e33fc61
Shinya Morino <shin.morino@gmail.com>
2018-07-28 18:08:35 +0900

    Adding build scripts for centos.

---
+++ build_centos6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export SQAODVER=0.3.1
+
+export CUDAVER=9.0
+docker run -ti --rm -v`pwd`/rpmbuild:/root/rpmbuild -e CUDAVER=${CUDAVER} -e SQAODVER=${SQAODVER} shinmorino/sqaod_buildenv:centos6 bash #-c 'cd /home/build_rpm; ./build_sse2.sh; ./build_avx2.sh'
+
+#export CUDAVER=9.2
+#docker run -ti --rm -v`pwd`/build_deb:/home/build_deb -e CUDAVER=${CUDAVER} -e SQAODVER=${SQAODVER}# shinmorino/sqaod_buildenv:xenial bash -c 'cd /home/build_deb; ./build_cuda.sh;'
