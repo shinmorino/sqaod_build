234469ac14729767bc9cfac0f35c99c56c959a58
Shinya Morino <shin.morino@gmail.com>
2018-09-03 18:50:19 +0900

    using VERSION to set package version.

--- build_centos7.sh
+++ build_centos7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SQAODVER=0.3.1
+SQAODVER=`cat VERSION`
 sudo chown root:root -R rpmbuild
 docker run -ti --rm -v`pwd`/rpmbuild:/root/rpmbuild shinmorino/sqaod_buildenv:centos7 bash -c 'cd /root/rpmbuild/SPECS; ./gen_spec.sh; rpmbuild -ba libsqaodc-sse2-cuda-9-0-devel.spec; rpmbuild -ba libsqaodc-avx2.spec; rpmbuild -ba libsqaodc-sse2-cuda-9-2.spec; '
 
