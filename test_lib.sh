fc7956c5d0736d74efd15b4886498d93ba4061c8
Shinya Morino <shin.morino@gmail.com>
2018-09-03 18:45:31 +0900

    library update scripts updated.

--- test_lib.sh
+++ test_lib.sh
@@ -6,9 +6,9 @@ simd=${2:-sse2}
 cudaver=${3:-9.0}
 
 container_name=sqaod-test-${dist}-${simd}-${cudaver}
-docker rm ${container_name}
+docker rm ${container_name} > /dev/null 2>&1
 
-docker run -ti -v`pwd`/tests:/home/tests -e SIMD=${simd} -e CUDAVER=${cudaver} -e SQAODVER=${version} --name ${container_name}  shinmorino/sqaod_buildenv:${dist} bash -c 'cd /home/tests; ./test_lib.sh $SQAODVER $SIMD $CUDAVER'
+docker run --runtime=nvidia -ti -v`pwd`/tests:/home/tests -e SIMD=${simd} -e CUDAVER=${cudaver} -e SQAODVER=${version} --name ${container_name}  shinmorino/sqaod_buildenv:${dist} bash -c 'cd /home/tests; ./test_lib.sh $SQAODVER $SIMD $CUDAVER'
 
 #export CUDAVER=9.2
 #docker run -ti --rm -v`pwd`/build_deb:/home/build_deb -e CUDAVER=${CUDAVER} -e SQAODVER=${SQAODVER}# shinmorino/sqaod_buildenv:xenial bash -c 'cd /home/build_deb; ./build_cuda.sh;'
--- test_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SQAODVER=`cat VERSION`
-export SQAODVER
-
-export CUDAVER=9.0
-export simd=sse2
-docker run --runtime=nvidia -ti --rm -v`pwd`/build_deb:/home/build_deb -e CUDAVER=${CUDAVER} -e SQAODVER=${SQAODVER} -e simd=${simd} shinmorino/sqaod_buildenv:artful bash -c 'cd /home/build_deb; ./build_${simd}.sh; bash'
